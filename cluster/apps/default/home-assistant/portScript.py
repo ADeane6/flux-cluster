0421e6d15c60a9064b81310bf1d5a0512038d16c
Ashley Deane <ashley.deane@personio.de>
2024-03-24 10:55:15 +0000

    add udp ports for home assistant via patch

---
+++ cluster/apps/default/home-assistant/portScript.py
@@ -0,0 +1,30 @@
+start_port = 10400
+end_port = 10450
+ports = range(start_port, end_port + 1)
+
+with open("load-balancer.yaml", "w") as file:
+  file.write('''
+apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: home-assistant
+  namespace: default
+spec:
+  values:
+    service:
+      lb:
+        type: LoadBalancer
+        loadBalancerIP: "${HASS_LB}"
+        externalTrafficPolicy: Local
+        ports:
+          http-lb:
+            enabled: true
+            port: 8123
+            protocol: HTTP
+          hue-lb:
+            port: 80''')
+  for port in ports:
+    file.write(f'''
+          udp-{port}:
+            port: {port}
+            protocol: UDP''')
